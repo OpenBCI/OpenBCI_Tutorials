720111f32568dd58b6c9ca8a7b1c278beb83a43f
evaesteban <eva.p.estebanvelasco@ieee.org>
2020-07-22 17:53:18 -0400

    added python scripts for Slideshow and Music tutorials

---
+++ EMG_Controlled_Slideshow/LSL_Stream_Slides.py
@@ -0,0 +1,26 @@
+"""Code modified from the example program to show how to read a multi-channel time series from LSL at https://github.com/OpenBCI/OpenBCI_GUI/blob/master/Networking-Test-Kit/LSL/lslStreamTest.py."""
+
+from pylsl import StreamInlet, resolve_stream
+import pyautogui
+import time  
+
+# resolve an EMG stream on the lab network and notify the user
+print("Looking for an EMG stream...")
+streams = resolve_stream('type', 'EMG')
+inlet = StreamInlet(streams[0])
+print("EMG stream found!")
+
+# initialize time threshold and variables for storing time 
+thres = 2000
+prev_time = 0
+
+while True:
+
+	sample, timestamp = inlet.pull_sample() # get EMG data sample and its timestamp
+
+	curr_time = int(round(time.time() * 1000)) # get current time in milliseconds
+
+	if ((sample[0] >=  1.0) & (curr_time - thres > prev_time)):  # if a blink is detected and enough time has gone by since the last blinking, press space
+		prev_time = int(round(time.time() * 1000)) # update time 
+		pyautogui.press('space')
+		
