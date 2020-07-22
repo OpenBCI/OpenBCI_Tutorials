cd22dbda80dfaf15a13a896c02c51892a2cc8b90
evaesteban <eva.p.estebanvelasco@ieee.org>
2020-07-22 17:55:15 -0400

    added python scripts for Scrolling tutorial

---
+++ EMG_Scrolling/LSL_Stream_Scroll.py
@@ -0,0 +1,33 @@
+"""Code modified from the example program to show how to read a multi-channel time series from LSL at https://github.com/OpenBCI/OpenBCI_GUI/blob/master/Networking-Test-Kit/LSL/lslStreamTest.py."""
+
+from pylsl import StreamInlet, resolve_stream
+import pyautogui
+import time  
+
+# resolve an EMG stream on the lab network and notify the user
+print("Looking for an EMG stream...")
+streams = resolve_stream('type', 'EMG')
+inlet = StreamInlet(streams[0])
+print("EMG stream found!")
+
+# initialize variables for storing time and threshold
+thres = 10
+prev_time = 0
+
+while True:
+
+	sample, timestamp = inlet.pull_sample() # get EMG data sample and its timestamp
+
+	curr_time = int(round(time.time() * 1000)) # get current time in milliseconds
+
+
+	if (((sample[1] == 1.0) or (sample[0] == 1.0))): # if an EMG peak is detected from any of the arms 
+
+		prev_time = int(round(time.time() * 1000)) # update time 
+		
+		if(sample[1] > sample[0]): # scroll up or down depending on which peak is larger
+			pyautogui.scroll(50)
+		else:
+			pyautogui.scroll(-50)
+
+		
